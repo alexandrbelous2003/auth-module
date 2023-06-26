c5050c7c84c8e26f2dbfb74c96d58d2369e1b077
Alexandr Belous <aa9499196@gmail.com>
2023-06-26 19:28:45 +0400

    feat: fix

--- src/app/posts/posts-material.module.ts
+++ src/app/posts/posts-material.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   exports: [
-    MatButtonModule,
     MatCheckboxModule,
     MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
     MatInputModule
   ],
 })
